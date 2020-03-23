6655ff52ae5afab9f9e7e3ed17406c3d68dbd56f
emmavalme <emma.valme@gmail.com>
2020-03-24 09:58:30 +1100

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Emma Välme on 2020-03-24.
+//  Copyright © 2020 Emma Välme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
